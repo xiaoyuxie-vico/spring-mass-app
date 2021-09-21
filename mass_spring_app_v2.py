cf5443105be270fab5dbb09c316f895fa9b41052
xiaoyu.xie <xiaoyuxie.vico@gmail.com>
2021-09-21 10:26:16 -0500

    udpate

--- mass_spring_app_v2.py
+++ mass_spring_app_v2.py
@@ -57,6 +57,7 @@ def main():
         unsafe_allow_html=True
     )
     #########################Objectives#########################
+    st.markdown('<p class="L2">developed by Xiaoyu Xie and Zhengtao Gan.</p>', unsafe_allow_html=True)
 
     st.markdown('<p class="L1">Objectives</p>', unsafe_allow_html=True)
 
